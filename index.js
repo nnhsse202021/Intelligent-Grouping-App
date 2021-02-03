f0844dcccca937180c26dbc8ab781b1b4dbe6ff6
ramganesan24 <54179781+ramganesan24@users.noreply.github.com>
2021-02-03 10:24:14 -0600

    Connecting server to Database
    We connected the server to our database.

---
+++ index.js
@@ -0,0 +1,39 @@
+// Imports \\
+const app = require('express')();
+const http = require('http').createServer(app);
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
+// Constants \\
+const DBName = "GroupingDB"
+const URI = `mongodb+srv://GroupingApp:${process.env.DBPASS}@groupingapp.iz1de.mongodb.net/GroupingDB?retryWrites=true&w=majority`
+
+// Express \\
+app.use(bodyParser.json())
+
+// '/' endpoint handling
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+//serving files
+app.use((req, res) => {
+  res.sendFile(__dirname + req.url)
+})
+
+http.listen(3000, function(){
+	console.log('listening on *:3000')
+})
+
+
+// Database \\
+
+
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+client.connect(err => {
+  const collection = client.db("GroupingDB").collection("userData")
+  collection.insertOne({id: 1, count: 5}, (err, res) => {
+    if (err) console.log(err)
+    else console.log(res)
+  })
+})
